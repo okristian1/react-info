a6b9a85bfca8492c339e9b9337d8c6a528e39e55
Ole Kristian Skramstad <okristian1@gmail.com>
2016-12-16 15:19:42 +0100

    snow

--- src/index.js
+++ src/index.js
@@ -2,77 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
 import './style/index.css';
-
-
-function addZero(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
-}
-function handleTime(unix) {
-  var newDate = new Date(unix.slice(6,16) * 1000);
-  var h = addZero(newDate.getHours());
-  var m = addZero(newDate.getMinutes());
-  var time = h + ':' + m
-  return time
-}
-
-var rooms = {10: "Lars Tiller"};
-
-function handleRoomName(num) {
-  for (var i = 0; i < 10; ++i) {
-    if (rooms) {
-      return rooms[num];
-    }
-  }
-}
-
-class MeetingRow extends React.Component {
-  render() {
-    this.props.meeting.StartDateTime = handleTime(this.props.meeting.StartDateTime)
-    this.props.meeting.EndDateTime = handleTime(this.props.meeting.EndDateTime)
-    this.props.meeting.TableNrs = handleRoomName(this.props.meeting.TableNrs)
-
-    if (!this.props.meeting.Company)
-      this.props.meeting.Company = this.props.meeting.CustomerName
-    return (
-      <tr>
-        <td>{this.props.meeting.Company}</td>
-        <td className="text-center">{this.props.meeting.StartDateTime} - {this.props.meeting.EndDateTime}</td>
-        <td className="text-center">{this.props.meeting.TableNrs}</td>
-      </tr>
-    );
-  }
-}
-
-
-class MeetingsTable extends React.Component {
-  render() {
-    var rows = [];
-    this.props.meetings.forEach((meeting) => {
-      rows.push(<MeetingRow meeting={meeting} key={meeting.Company} />);
-    });
-    return (
-      <div className="container">
-      <img className="pull-right" src={require('./style/smnlogo.png')}alt={"SMN LOGO"} width={"20%"}  />
-      <table>
-        <thead>
-          <tr>
-            <th>Møter i dag</th>
-            <th className="text-center">Tidspunkt</th>
-            <th className="text-center">Møterom</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
-      </div>
-    );
-  }
-}
-
-
-var PRODUCTS = [
+var meetings = [
 {
 "AreaName": "Møtesenteret",
 "Company": "Sparebanken 1 SMN ",
@@ -172,7 +102,85 @@ var PRODUCTS = [
 ]
 
 
+var rooms = {
+  1: "Amfiet", 
+  2: "Jacob roll", 
+  3: "Olav Tryggvasson", 
+  4: "Cicignon", 
+  5: "Tordenskjold",
+  8: "Thomas Angell",  
+  10: "Lars Tiller",
+  13: "Fru Schøller"
+};
+
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+function handleTime(unix) {
+  var newDate = new Date(unix.slice(6,16) * 1000);
+  var h = addZero(newDate.getHours());
+  var m = addZero(newDate.getMinutes());
+  var time = h + ':' + m
+  return time
+}
+
+function handleRoomName(num) {
+  if(num in rooms) {
+    return rooms[num];
+    }
+    return "ukjent";
+  }
+
+class MeetingRow extends React.Component {
+  render() {
+    this.props.meeting.StartDateTime = handleTime(this.props.meeting.StartDateTime)
+    this.props.meeting.EndDateTime = handleTime(this.props.meeting.EndDateTime)
+    this.props.meeting.TableNrs = handleRoomName(this.props.meeting.TableNrs)
+
+    if (!this.props.meeting.Company)
+      this.props.meeting.Company = this.props.meeting.CustomerName
+    return (
+      <tr>
+        <td>{this.props.meeting.Company}</td>
+        <td className="text-center">{this.props.meeting.StartDateTime} - {this.props.meeting.EndDateTime}</td>
+        <td className="text-right">{this.props.meeting.TableNrs}</td>
+      </tr>
+    );
+  }
+}
+
+
+class MeetingsTable extends React.Component {
+  render() {
+    var rows = [];
+    this.props.meetings.forEach((meeting) => {
+      if(meeting["TableNrs"] in rooms) {
+              rows.unshift(<MeetingRow meeting={meeting} key={meeting.Company} />);
+      }});
+    return (
+      <div className="container">
+      <img className="pull-right" src={require('./style/smnlogo.png')}alt={"SMN LOGO"} width={"30%"}  />
+      <table>
+        <thead>
+          <tr>
+            <th>Møte</th>
+            <th className="text-center">Tidspunkt</th>
+            <th className="text-right">Sted</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+      </div>
+    );
+  }
+}
+
+
 ReactDOM.render(
-  <MeetingsTable meetings={PRODUCTS} />,
+  <MeetingsTable meetings={meetings} />,
   document.getElementById('root')
 );
